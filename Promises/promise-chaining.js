246c0aea60870e3f6b967bae463479eeca911303
Derrick Holleman <derrickholleman@gmail.com>
2021-09-14 23:41:12 -0500

    style and create object

--- Promises/promise-chaining.js
+++ Promises/promise-chaining.js
@@ -1,25 +1,27 @@
-let user1 = 'derrickholleman'
-let user2 = 'Carpe-liam'
-let user3 = 'miguelh72'
+let allUsers = {
+    user1: 'derrickholleman',
+    user2: 'Carpe-liam',
+    user3: 'miguelh72'
+}
 
 let user1El = document.getElementById('user1El')
 let user2El = document.getElementById('user2El')
 let user3El = document.getElementById('user3El')
 
 function fetchGithub() {
-    fetch(`https://api.github.com/users/${user1}`)
+    fetch(`https://api.github.com/users/${allUsers.user1}`)
     .then(response => response.json())
     .then((user) => {
         user1El.textContent = `${user.name} lives in ${user.location} and has ${user.public_repos} public repositories`
         // queues up next fetch with a return 
-        return fetch(`https://api.github.com/users/${user2}`)
+        return fetch(`https://api.github.com/users/${allUsers.user2}`)
     })
     // next fetch's data
     .then(response => response.json())
     .then((user) => {
         user2El.textContent = `${user.name} lives in ${user.location} and has ${user.public_repos} public repositories`
 
-        return fetch(`https://api.github.com/users/${user3}`)
+        return fetch(`https://api.github.com/users/${allUsers.user3}`)
     })
     .then(response => response.json())
     .then((user) => {
