c6a575d3eb7b200b1cb872cd7be2f8b5328c3a4a
Derrick Holleman <derrickholleman@gmail.com>
2021-09-22 10:51:24 -0500

    form functioning

--- rendering-with-fetch/index.js
+++ rendering-with-fetch/index.js
@@ -1,17 +1,42 @@
 const textContainer = document.getElementById("text-container");
+const inputEl = document.getElementById("amount");
+
+// prevent default on form submit
+const amountForm = document.getElementById('amountForm')
+amountForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+}, true);
+
+let amount = 0;
 
 async function getUsers(num) {
   const url = `https://randomuser.me/api/?results=${num}&nat=us`;
   try {
     const response = await fetch(url);
     const data = await response.json();
-    data.results.map((person) => {
-       textContainer.innerHTML += `
-        <p>${person.name.first} ${person.name.last} lives in ${person.location.city}, ${person.location.state}
-        `;
-    });
+    return data;
   } catch (err) {
     console.error(err);
   }
 }
-getUsers(5);
+
+async function renderUsers() {
+  // passes in the number from localStorage
+  const data = await getUsers(JSON.parse(localStorage.getItem("amount")));
+  data.results.map((person) => {
+    textContainer.innerHTML += `
+     <p>${person.name.first} ${person.name.last} lives in ${person.location.city}, ${person.location.state}
+     `;
+  });
+}
+
+function submitForm() {
+  if (inputEl.value.length === 0) {
+    return null
+  }
+    textContainer.innerHTML = "";
+    amount = inputEl.value;
+    localStorage.setItem("amount", JSON.stringify(amount));
+    renderUsers();
+    inputEl.value = ''
+}
