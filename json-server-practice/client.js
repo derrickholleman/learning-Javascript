99a857dc6fcf71e4f5b013c4d7fb499a74f01983
Derrick Holleman <derrickholleman@gmail.com>
2021-09-29 14:04:13 -0500

    json server update

--- json-server-practice/client.js
+++ json-server-practice/client.js
@@ -1,17 +1,18 @@
 const axios = require("axios");
-const url = "http://localhost:5000/users";
+const userURL = "http://localhost:5000/users";
+const carURL = 'http://localhost:5000/cars'
 
 function getUsers() {
-  return axios.get(url).then((response) => console.log(response.data));
+  return axios.get(userURL).then((response) => console.log(response.data));
 }
 // getUsers()
 
 function createUser() {
-    return axios.post(url, {
-        "id": 4,
-        "name": "Darryl",
-        "city": "Denver",
-        "age": 66
+    return axios.post(userURL, {
+        // id gets added automatically
+        "name": "Mickey",
+        "city": "Orlanda",
+        "age": 50
     })
     .then((response) => console.log(response.data))
     .catch((err) => console.error(err))
@@ -19,19 +20,37 @@ function createUser() {
 // createUser()
 
 function deleteUser(id) {
-    return axios.delete(`${url}/${id}`)
+    return axios.delete(`${userURL}/${id}`)
     .then(() => console.log(`deleted user ${id} successfully`))
     .catch((err) => console.error(err))
 }
 // deleteUser(4)
 
 function updateUser(id) {
-    return axios.put(`${url}/${id}`, {
-        "name": "Paul",
-        "city": "New York City",
-        "age": 60
+    return axios.put(`${userURL}/${id}`, {
+        "name": "Mickey",
+        "city": "Orlando",
+        "age": 50
     })
     .then(() => console.log(`updated user ${id} successfully`))
     .catch((err) => console.error(err))
 }
-// updateUser(8)
+// updateUser(9)
+
+function getCars() {
+    return axios.get(carURL)
+    .then((response) => console.log(response.data))
+    .catch((err) => console.error(err))
+}
+// getCars()
+
+function createCar() {
+    return axios.post(carURL, {
+        // id gets added automatically
+        "make": "Ford",
+        "model": "F150"
+    })
+    .then(() => console.log('car successfully added'))
+}
+// createCar()
+
